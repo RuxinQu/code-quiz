578dcd35adcbfc6b92fe3ab71e14728a41efb1cd
Ruxin Qu <rosheenqu@gmail.com>
2022-08-31 15:40:29 -0700

    fix the bug when click the wrong answer, the page freezes

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,59 +2,44 @@ $(document).ready(() => {
     let questions = [
         {
             question: 'Commonly used data types DO Not include:',
-            choice1: "strings",
-            choice2: 'boolean',
-            choice3: 'alerts',
-            choice4: 'numbers',
-            answer: 3
+            choices: ['1. strings', '2. boolean', '3. alerts', '4. numbers'],
+            answer: 2
         },
         {
             question: 'The condition in an if / else statement is enclosed with________',
-            choice1: 'quotes',
-            choice2: 'curly brackets',
-            choice3: 'parenthesis',
-            choice4: 'square brackets',
-            answer: 3
+            choices: ['1. quotes', '2. curly brackets', '3. parenthesis', '4. square brackets'],
+            answer: 2
         },
         {
             question: 'Array in JavaScript can be used to store________',
-            choice1: 'numbers and strings',
-            choice2: 'other arrays',
-            choice3: 'booleans',
-            choice4: 'all of the above',
-            answer: 4
-        },
-        {
-            question: 'String values must be enclosed within________when being assigned to variables.',
-            choice1: 'commas',
-            choice2: 'curly brackets',
-            choice3: 'quotes',
-            choice4: 'parenthesis',
+            choices: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
             answer: 3
         },
         {
+            question: 'String values must be enclosed within________when being assigned to variables.',
+            choices: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis'],
+            answer: 2
+        },
+        {
             question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
-            choice1: 'JavaScript',
-            choice2: 'terminal/bash',
-            choice3: 'for loops',
-            choice4: 'console.log',
-            answer: 4
+            choices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
+            answer: 3
         }
     ];
 
     let questionCount = 0;
     let timeTotal = 76;
-    let correctAnswer;
-
     loadQAndA(questionCount);
 
-    $('.choice-container').on('click', (event) => {
-        if (questionCount > 4) {
-            stopTimer();
-        } else {
-            checkAnswer(event);
+
+
+    $('.choice-container p').on('click', (event) => {
+        checkAnswer(event);      
+        if (questionCount < 4) {
             questionCount++;
             loadQAndA(questionCount);
+        } else {
+            stopTimer();      
         }
     })
 
@@ -69,24 +54,37 @@ $(document).ready(() => {
     }, 1000)
 
     const stopTimer = () => {
-        clearInterval(timer);
         $('.container').addClass('hide');
         $('.score-container').removeClass('hide').addClass('show');
         $('#score').text(timeTotal);
+        clearInterval(timer);
     }
 
     function loadQAndA(questionCount) {
         $('#question').text(questions[questionCount].question);
-        $('#choice1').text(questions[questionCount].choice1);
-        $('#choice2').text(questions[questionCount].choice2);
-        $('#choice3').text(questions[questionCount].choice3);
-        $('#choice4').text(questions[questionCount].choice4);
+        let choicesElArr = [$('#choice1'), $('#choice2'), $('#choice3'), $('#choice4')];
+
+        for (let x = 0; x < choicesElArr.length; x++) {
+            choicesElArr[x].text(questions[questionCount].choices[x])
+        }
+
     }
 
     const checkAnswer = (event) => {
-        console.log(questionCount)
-        $('.result-container').addClass('show').removeClass('hide');
-        $('.result-text').text(event.target.dataset.number == questions[questionCount].answer ? 'Correct!' : `Wrong! The correct answer is ${questions[questionCount].answer}`)
+        showResult()
+        console.log(questions[questionCount].answer)
+        if (event.target.dataset.number == questions[questionCount].answer) {
+            $('.result-text').text('Correct!')
+        } else {
+            $('.result-text').text('Wrong!')
+            timeTotal = timeTotal - 10;
+        }
     }
 
+    const showResult = () => {
+        $('.result-container').addClass('show').removeClass('hide');
+    }
+    const hideResult = () => {
+        $('.result-container').addClass('hide').removeClass('show');
+    }
 })
